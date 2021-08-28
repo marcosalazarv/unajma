d4417e0f1ece8ceb01a799043aa397daf15921c0
marcosalazarv <106020191@unajma.edu.pe>
2021-08-28 15:00:05 -0500

    colocando estilos

--- likes.php
+++ likes.php
@@ -32,3 +32,112 @@
 	}
  
 ?>
+
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>People who like | Mybook</title>
+	</head>
+
+	<style type="text/css">
+		
+		#blue_bar{
+
+			height: 50px;
+			background-color: #405d9b;
+			color: #d9dfeb;
+
+		}
+
+		#search_box{
+
+			width: 400px;
+			height: 20px;
+			border-radius: 5px;
+			border:none;
+			padding: 4px;
+			font-size: 14px;
+			background-image: url(search.png);
+			background-repeat: no-repeat;
+			background-position: right;
+
+		}
+
+		#profile_pic{
+
+			width: 150px;
+			border-radius: 50%;
+			border:solid 2px white;
+		}
+
+		#menu_buttons{
+
+			width: 100px;
+			display: inline-block;
+			margin:2px;
+		}
+
+		#friends_img{
+
+			width: 75px;
+			float: left;
+			margin:8px;
+
+		}
+
+		#friends_bar{
+
+			min-height: 400px;
+			margin-top: 20px;
+			padding: 8px;
+			text-align: center;
+			font-size: 20px;
+			color: #405d9b;
+		}
+
+		#friends{
+
+		 	clear: both;
+		 	font-size: 12px;
+		 	font-weight: bold;
+		 	color: #405d9b;
+		}
+
+		textarea{
+
+			width: 100%;
+			border:none;
+			font-family: tahoma;
+			font-size: 14px;
+			height: 60px;
+
+		}
+
+		#post_button{
+
+			float: right;
+			background-color: #405d9b;
+			border:none;
+			color: white;
+			padding: 4px;
+			font-size: 14px;
+			border-radius: 2px;
+			width: 50px;
+		}
+ 
+ 		#post_bar{
+
+ 			margin-top: 20px;
+ 			background-color: white;
+ 			padding: 10px;
+ 		}
+
+ 		#post{
+
+ 			padding: 4px;
+ 			font-size: 13px;
+ 			display: flex;
+ 			margin-bottom: 20px;
+ 		}
+
+	</style>
